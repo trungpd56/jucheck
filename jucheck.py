62d090ab7fd0e899e240eeb07af4eb92900f2ed9
trungpd56 <trungpd56@gmail.com>
2022-03-14 10:18:20 +0700

    update to static method and do check in one loop

--- jucheck.py
+++ jucheck.py
@@ -5,20 +5,15 @@ from termcolor import colored
 
 
 class judev:
-    def __init__(self,ip,user,password):
+    def __init__(self, ip, user, password):
         self.ip = ip
         self.user = user
         self.password = password
 
-    def getrpc(self,rpc):
-        rpc = rpc.replace('-','_')
-        with Device(host=self.ip,user=self.user,password=self.password) as dev:
-            return eval(f'dev.rpc.get_{rpc}()')
-
-    def get_intinfo(self):
-        x = self.getrpc('configuration')
+    @staticmethod
+    def get_intinfo(x):
         focus = x.findall('.//interfaces/interface')
-        self.intinfo = dict()
+        intinfo = dict()
         for i in focus:
             interface = i.find('name').text
             if i.find('description') is not None:
@@ -32,71 +27,81 @@ class judev:
                 else:
                     description = descriptor0
                 ip = i.find('.//address/name').text
-                self.intinfo[subinterface] = (description, ip)
-        return self.intinfo
+                intinfo[subinterface] = (description, ip)
+        return intinfo
 
     def check_isis(self):
-        while True:
-            self.get_isadj()
-            self.get_isconf()
-            self.get_intinfo()
-            judev.print_nei(self.isconf,self.isadj,self.intinfo)
-            time.sleep(1)
-            # os.system('clear')
+        with Device(self.ip, user=self.user, password=self.password) as dev:
+            while True:
+                os.system('clear')
+                isadj = judev.get_isadj(
+                    dev.rpc.get_isis_adjacency_information())
+                isconf = judev.get_isconf(dev.rpc.get_configuration())
+                intinfo = judev.get_intinfo(dev.rpc.get_configuration())
+                judev.print_nei(isconf, isadj, intinfo)
+                time.sleep(3)
 
-    def check_ospf(self):
-        while True:
-            os.system('clear')
-            self.get_osnei()
-            self.get_osconf()
-            self.get_intinfo()
-            judev.print_nei(self.osconf,self.osnei,self.intinfo)
-            time.sleep(1)
-
-    def get_isadj(self):
-        x = self.getrpc('isis-adjacency-information')
+    @staticmethod
+    def get_isadj(x):
         adj = x.findall('.//isis-adjacency')
-        self.isadj = dict()
+        isadj = dict()
         for i in adj:
             interface = i.find('interface-name').text
             neighbor = i.find('system-name').text
             state = i.find('adjacency-state').text
-            self.isadj[interface] = (neighbor, state)
-        return self.isadj
+            isadj[interface] = (neighbor, state)
+        return isadj
 
-    def get_isconf(self):
-        x = self.getrpc('configuration')
+    @staticmethod
+    def get_isconf(x):
         interface = x.findall('.//isis/interface')
-        self.isconf = sorted({i.find('name').text for i in interface})
-        return self.isconf
+        isconf = sorted({i.find('name').text for i in interface})
+        return isconf
 
-    def get_osnei(self):
-        x = self.getrpc('ospf-neighbor-information')
+    def check_ospf(self):
+        with Device(self.ip, user=self.user, password=self.password) as dev:
+            while True:
+                os.system('clear')
+                osnei = judev.get_osnei(
+                    dev.rpc.get_ospf_neighbor_information())
+                osconf = judev.get_osconf(dev.rpc.get_configuration())
+                intinfo = judev.get_intinfo(dev.rpc.get_configuration())
+                judev.print_nei(osconf, osnei, intinfo)
+                time.sleep(3)
+
+    @staticmethod
+    def get_osnei(x):
         adj = x.findall('.//ospf-neighbor')
-        self.osnei = dict()
+        osnei = dict()
         for i in adj:
             interface = i.find('interface-name').text
             neighbor = i.find('neighbor-id').text
             state = i.find('ospf-neighbor-state').text
-            self.osnei[interface] = (neighbor, state)
-        return self.osnei
-
-    def get_osconf(self):
-        x = self.getrpc('configuration')
-        interface = x.findall('.//area/interface')
-        self.osconf = sorted({i.find('name').text for i in interface})
-        return self.osconf
-
-
+            osnei[interface] = (neighbor, state)
+        return osnei
 
     @staticmethod
-    def print_nei(conf,neighbor,intinfo):
+    def get_osconf(x):
+        interface = x.findall('.//area/interface')
+        osconf = sorted({i.find('name').text for i in interface})
+        return osconf
+
+    @staticmethod
+    def print_nei(conf, neighbor, intinfo):
         for i in conf:
             if 'lo' not in i:
                 if i in neighbor.keys():
                     if neighbor[i][1] == 'Up':
-                        print(colored(f"[+] Interface {i:14s} | {neighbor[i][0]:16s} | {neighbor[i][1]:14s} | {intinfo[i][0]:15s} | {intinfo[i][1]}",'blue'))
+                        print(colored(
+                            f"[+] Interface {i:14s} | {neighbor[i][0]:16s} | {neighbor[i][1]:14s} | {intinfo[i][0]:15s} | {intinfo[i][1]}", 'blue'))
                     else:
-                        print(f"[*] Interface {i:14s} | {neighbor[i][0]:16s} | {neighbor[i][1]:14s} | {intinfo[i][0]:15s} | {intinfo[i][1]}")
+                        print(
+                            f"[*] Interface {i:14s} | {neighbor[i][0]:16s} | {neighbor[i][1]:14s} | {intinfo[i][0]:15s} | {intinfo[i][1]}")
                 else:
-                    print(colored(f"[-] Interface {i:14s} | {'UNKNOWN':16s} | {'UNKNOWN':14s} | {intinfo[i][0]:15s} | {intinfo[i][1]}",'red'))
+                    print(colored(
+                        f"[-] Interface {i:14s} | {'UNKNOWN':16s} | {'UNKNOWN':14s} | {intinfo[i][0]:15s} | {intinfo[i][1]}", 'red'))
+
+
+if __name__ == '__main__':
+    r5 = judev('172.16.1.5', 'root', 'root123')
+    r5.check_ospf()
